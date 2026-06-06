1ba46f812e28e1c376e0ef50d9d655e223a4bc07
Stephane Popinet <popinet@basilisk.fr>
2026-06-06 09:25:29 +0000

    Compilation and linking of the HIP/CUDA libraries

--- src/grid/hip/multigrid.h
+++ src/grid/hip/multigrid.h
@@ -52,7 +52,13 @@ typedef struct {
   } while(0) @
 @endif
 
-#pragma autolink -L$BASILISK/grid/cuda -lbuda -lcuda -lnvrtc -L$BASILISK/grid/gpu -lerrors
+/**
+Note that the libraries `-lcuda -lcudart -lnvrtc` are only required for the
+HIP-on-CUDA version. This will need to be adapted for the HIP/ROCm
+version. See also the [Makefile]() for compilation of the
+corresponding `libhip.a` library. */
+
+#pragma autolink -L$BASILISK/grid/hip -lhip -lcuda -lcudart -lnvrtc -L$BASILISK/grid/gpu -lerrors
 
 static void hip_multigrid_methods()
 {
